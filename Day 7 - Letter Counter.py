89a87e22629d7c5738fb658eaf2ffb1709fb5d11
nzmksk <mhafiz9676@gmail.com>
2022-10-01 20:09:40 +0800

    Added Day 7 challenge.

---
+++ Day 7 - Letter Counter.py
@@ -0,0 +1,15 @@
+# Create a function that counts all the letters in a given input text
+
+def letter_counter(text):
+    # Create a list ranging from a-z
+    letters = list(map(chr, range(ord('a'), (ord('z') + 1))))
+    count = 0
+    for alphabet in text:
+        if alphabet in letters:
+            count += 1
+    return count
+
+
+# Lowercase all letters
+text = input("Insert your text here: ").lower()
+print(letter_counter(text))
