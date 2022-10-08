a8884e6e344f9f6d56b2113296a2de2f67576fd2
Mohd Hafiz Zabba <mhafiz9676@gmail.com>
2022-10-09 00:00:26 +0800

    Added Day 13 challeenge.

---
+++ Day 13 - Largest Palindrome Product.py
@@ -0,0 +1,18 @@
+# A palindromic number reads the same both ways.
+# The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 x 99.
+
+# Find the largest palindrome made from the product of two n-digit numbers.
+
+def largest_palindrome(n):
+    def interval(n):
+        return range(10**(n-1), 10**n)
+
+    largest_palindrome = None
+    for i in interval(n):
+        for j in interval(n):
+            product = i * j
+            if str(product) == str(product)[::-1]:
+                if largest_palindrome == None or largest_palindrome < product:
+                    largest_palindrome = product
+
+    return largest_palindrome
